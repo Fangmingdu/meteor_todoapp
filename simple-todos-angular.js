0bc2e8f57355728d9184cd24196b5c7ada649f41
Fangming Du <fangmdu@gmail.com>
2015-08-14 14:16:34 -0500

    add filtering and ui changes

--- simple-todos-angular.js
+++ simple-todos-angular.js
@@ -7,9 +7,13 @@ if (Meteor.isClient) {
   angular.module('simple-todos').controller('TodosListCtrl', ['$scope','$meteor',
     function ($scope, $meteor) {
  
-     $scope.tasks = $meteor.collection( function() {
-        return Tasks.find({}, { sort: { createdAt: -1 } })
+     $scope.tasks = $meteor.collection(function() {
+        return Tasks.find($scope.getReactively('query'), {sort: {createdAt: -1}})
       });
+      
+      $scope.incompleteCount = function () {
+        return Tasks.find({ checked: {$ne: true} }).count();
+      };
       
       $scope.addTask = function (newTask) {
         $scope.tasks.push( {
@@ -17,6 +21,13 @@ if (Meteor.isClient) {
           createdAt: new Date() }
         );
       };
+ 
+      $scope.$watch('hideCompleted', function() {
+        if ($scope.hideCompleted)
+          $scope.query = {checked: {$ne: true}};
+        else
+          $scope.query = {};
+      });
  
   }]);
 }
